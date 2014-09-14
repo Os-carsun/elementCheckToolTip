0e22c5205e7f70d39d1a3aad86c16aaebbd9f314
Os-carsun <good0991@gmail.com>
2014-09-14 11:14:25 +0800

    beta 1.0

--- js/main.js
+++ js/main.js
@@ -11,30 +11,40 @@ function mouseTooltip(e) {
 
     mouse.x = e.clientX || e.pageX;
     mouse.y = e.clientY || e.pageY;
-    console.log(">>"+mouseClick+"<<");
+    console.log(">>x:"+mouse.x+"y:"+mouse.y+"<<");
     if (mouseClick === true) return;
     currentElement = document.elementFromPoint(mouse.x, mouse.y);
-    console.log(currentElement);
+    // console.log(currentElement);
     var content = "";
 
+    var span = document.createElement('span');
+    span.setAttribute("class","coupontooltip");
+    document.body.appendChild(span);
 
-
+    span.style.top=mouse.y;
+    span.style.left=mouse.x;
     for (var i = 0, atts = currentElement.attributes, n = atts.length, arr = []; i < n; i++) {
-        content += atts[i].nodeName + " : <input type='text' name=" + atts[i].nodeName + " value= ' "  + atts[i].value + "'> <br>";
+        span.innerHTML+=atts[i].nodeName + " : <input type='text' name=" + atts[i].nodeName + " value= ' "  + atts[i].value + "'> <br>";
+        //  document.getElementsByName(atts[i].nodeName)[0].onchange = function(e) {
+        //     currentElement.setAttribute(e.srcElement.name, e.srcElement.value);
+        // }
     }
-    content += "innerTexture : <input type='text' name='innerText' value='" + currentElement.innerText + "'> <br>";
-    currentElement.innerHTML += " <span class=" + "coupontooltip" + ">" + content + "</span>";
-    for (var i = 0, atts = currentElement.attributes, n = atts.length, arr = []; i < n; i++) {
-        var node = atts[i];
-        document.getElementsByName(node.nodeName)[0].onchange = function(e) {
-            console.log(e.srcElement.value);
-            currentElement.setAttribute(e.srcElement.name, e.srcElement.value);
-        }
-    }
+    span.innerHTML+= "innerTexture : <input type='text' name='innerText' value='" + currentElement.innerText + "'> <br>";
     document.getElementsByName("innerText")[0].onchange = function(e) {
         currentElement.innerText = document.getElementsByName("innerText")[0].value;
         mouseClick = false;
     }
+    currentElement.parentElement.innerHTML += " <span class=" + "coupontooltip" + ">" + content + "</span>";
+    for (var i = 0, atts = currentElement.attributes, n = atts.length, arr = []; i < n; i++) {
+        var node = atts[i];
+        document.getElementsByName(node.nodeName)[0].onchange = function(e) {
+            currentElement.setAttribute(e.srcElement.name, e.srcElement.value);
+        }
+    }
+    // document.getElementsByName("innerText")[0].onchange = function(e) {
+    //     currentElement.innerText = document.getElementsByName("innerText")[0].value;
+    //     mouseClick = false;
+    // }
     mouseClick = true;
     document.getElementsByClassName('coupontooltip')[0].ondblclick = function() {
         document.getElementsByClassName('coupontooltip')[0].remove();
