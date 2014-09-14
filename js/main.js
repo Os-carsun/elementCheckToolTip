1143c372bcf6189e9eef0392b21bfb8b00a0be65
Knowlet <knowlet3389@gmail.com>
2014-09-14 10:59:45 +0800

    fix frame issue

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ var mouse = {
 };
 var mouseClick = false;
 var currentElement = null;
-var body = document.getElementsByTagName('body')[0];
 
 function mouseTooltip(e) {
 
     mouse.x = e.clientX || e.pageX;
     mouse.y = e.clientY || e.pageY;
+    console.log("x: " + mouse.x + ", y: " + mouse.y);
     console.log(">>"+mouseClick+"<<");
     if (mouseClick === true) return;
     currentElement = document.elementFromPoint(mouse.x, mouse.y);
@@ -19,12 +19,12 @@ function mouseTooltip(e) {
 
 
 
-    for (var i = 0, atts = currentElement.attributes, n = atts.length, arr = []; i < n; i++) {
+    for (var i = 0, atts = currentElement.attributes, n = atts.length; i < n; i++) {
         content += atts[i].nodeName + " : <input type='text' name=" + atts[i].nodeName + " value= ' "  + atts[i].value + "'> <br>";
     }
     content += "innerTexture : <input type='text' name='innerText' value='" + currentElement.innerText + "'> <br>";
     currentElement.innerHTML += " <span class=" + "coupontooltip" + ">" + content + "</span>";
-    for (var i = 0, atts = currentElement.attributes, n = atts.length, arr = []; i < n; i++) {
+    for (var i = 0, atts = currentElement.attributes, n = atts.length; i < n; i++) {
         var node = atts[i];
         document.getElementsByName(node.nodeName)[0].onchange = function(e) {
             console.log(e.srcElement.value);
@@ -42,3 +42,7 @@ function mouseTooltip(e) {
     }
 }
 document.onmousedown = mouseTooltip;
+for (var i = 0; i < frames.length; i++) {
+    frames[i].document.onmousedown = mouseTooltip;
+    console.log(i);
+}
